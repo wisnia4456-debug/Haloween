411850c556d27fdbab0b400562bee6b25ba65dc0
wisnia4456-debug <wisnia4456@gmail.com>
2026-09-05 17:15:15 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Wklej tutaj konfigurację ze swojego projektu Firebase.
+// Firebase Console -> Ustawienia projektu -> Twoje aplikacje -> SDK setup and configuration
+export const firebaseConfig = {
+  apiKey: "TWOJ_API_KEY",
+  authDomain: "TWOJ_PROJEKT.firebaseapp.com",
+  projectId: "TWOJ_PROJEKT",
+  storageBucket: "TWOJ_PROJEKT.appspot.com",
+  messagingSenderId: "TWOJ_SENDER_ID",
+  appId: "TWOJ_APP_ID"
+};
